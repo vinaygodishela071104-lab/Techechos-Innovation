29119391c58da3635eca1cd0d00f65cf6599cfbe
Eshwar04 <114127387+Eshwar04@users.noreply.github.com>
2026-06-12 17:51:41 +0530

    first commit

---
+++ careers.js
@@ -0,0 +1,25 @@
+
+/* =========================
+   DARK MODE
+========================= */
+
+document.addEventListener("click", (e) => {
+
+  if (e.target.closest(".dark-mode-btn")) {
+
+    document.body.classList.toggle("dark-mode");
+
+    localStorage.setItem(
+      "theme",
+      document.body.classList.contains("dark-mode")
+        ? "dark"
+        : "light"
+    );
+
+  }
+
+});
+
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark-mode");
+}
